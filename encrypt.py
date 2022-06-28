ecff68461c24377c2d7b9d8f0024c92ac499787d
Gisbertillo <albjcoello@gmail.com>
2022-06-28 18:40:34 -0400

    update

---
+++ encrypt.py
@@ -0,0 +1,10 @@
+# Read an input
+#input = input()
+
+# Allowed characters
+lower_letters = "abcdefghijklmnñopqrstuvwxyzáéíóúü"
+upper_letters = lower_letters.upper()
+numbers = "1234567890"
+spetial = " ,."
+char = lower_letters + upper_letters + numbers + spetial
+print(char)
